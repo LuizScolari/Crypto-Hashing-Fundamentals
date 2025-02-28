647007a5f0b0768251e271050aa6d2daf1f8c85f
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-02-28 17:38:41 -0400

    Implement Caesar cipher decryption function

--- Caesar Cipher/caesar_encrypt.py
+++ Caesar Cipher/caesar_encrypt.py
@@ -11,7 +11,25 @@ def caesar_encrypt(text):
             index = (index+KEY) % len(ALPHABET)
             cipher_text = cipher_text + ALPHABET[index]
         else:
-            cipher_text = cipher_text + " " 
+            cipher_text = cipher_text + " "   
     return cipher_text
 
-print(caesar_encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+def caesar_decrypt(text):
+    text = text.upper()
+    decrypt_text = ""
+
+    for l in text:
+        if l != " ":
+            index =  ALPHABET.find(l)
+            index = (index-KEY) % len(ALPHABET)
+            decrypt_text = decrypt_text + ALPHABET[index]
+        else:
+            decrypt_text = decrypt_text + " " 
+    return decrypt_text
+
+
+ciphered = caesar_encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print(f"Encrypted: {ciphered}")
+
+decrypt = caesar_decrypt(ciphered)
+print(f"Decrypted: {decrypt}")
