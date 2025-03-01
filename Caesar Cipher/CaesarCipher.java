638df0e49066e20526602df1d88268fad2ec554e
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-03-01 13:17:57 -0400

    Implementing Caesar Cipher decryption in java

--- Caesar Cipher/CaesarCipher.java
+++ Caesar Cipher/CaesarCipher.java
@@ -19,4 +19,35 @@ public class CaesarCipher{
         };
         return cipher_text;
     }
+    
+    public String decrypt(String text, int KEY){
+        String decrypt_text = "";
+        text = text.toUpperCase();
+
+        for(int i = 0; i < text.length(); i++){
+            String character = String.valueOf(text.charAt(i));
+            int index = ALPHABET.indexOf(character);
+            
+            if (character.equals(" ")){ 
+                decrypt_text = decrypt_text + " ";
+            } else {
+                index = (index - KEY) % 26;
+                decrypt_text = decrypt_text + String.valueOf(ALPHABET.charAt(index));
+            }
+        };
+        return decrypt_text;
+    }
 }
+
+/*
+EXAMPLE OF TEST
+public class main {
+    public static void main(String[] args) {
+        CaesarCipher cipher = new CaesarCipher();
+        String encrypted = cipher.encrypt("HELLO WORLD", 3);
+        System.out.println(encrypted); // "KHOOR ZRUOG"
+        String decrypt = cipher.decrypt("KHOOR ZRUOG", 3);
+        System.out.println(decrypt); // "HELLO WORLD"
+    }
+}
+ */
