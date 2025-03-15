0c3e485e4c0dea5f8f1daba8534bcac0d6cf0ffa
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-03-15 18:11:40 -0300

    Update des.py

--- DES/des.py
+++ DES/des.py
@@ -1,7 +1,7 @@
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
-from Crypto.Random import get_random_bytes
+import binascii
 
 key = b'mysecret'
 
@@ -9,4 +9,11 @@ cipher = DES.new(key, DES.MODE_CBC)
 
 plaintext = b'this is the original message'
 cipher_text = cipher.encrypt(pad(plaintext, DES.block_size))
-print(cipher_text)
+iv = cipher.iv
+
+decrypt_cipher = DES.new(key, DES.MODE_CBC, iv)
+original = decrypt_cipher.decrypt(cipher_text)
+original = unpad(original, DES.block_size)
+
+print(binascii.hexlify(cipher_text).decode())
+print(original.decode())
