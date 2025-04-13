202383f371fb98d3496af16b9c36f3fb9c6aeb4f
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-04-13 17:08:25 -0300

    Implementing Diffie-Hellman cryptosystem

---
+++ Diffie-Hellman key exchange/Diffie_Hellman.py
@@ -0,0 +1,28 @@
+import random
+
+def generate_private_key(n, g):
+    # Alice private key
+    a = random.randint(1,n)
+
+    # Bob private key
+    b = random.randint(1, n)
+
+    # gˆx mod n, Alice's k1
+    k1 = pow(g, a, n)
+
+    # gˆx mod n, Bob's k2
+    k2 = pow(g, b, n)
+
+    # If an atacker try to get x and y (the private keys) => the discrete logarithm problem
+    # It is exponentially slow process
+    print("Alice's shared key: ", pow(k2, a, n))
+    print("Bob's shared key: ", pow(k1, b, n))
+
+if __name__ == '__main__':
+    # Should be a huge prime number
+    n = 37
+    # Primitive root of n
+    g = 13
+
+    # Using Diffie-Hellman cryptosystem to generate the shared key (for DES and AES)
+    generate_private_key(n, g)
